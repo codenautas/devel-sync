cf5f8437b6cd3f899f80dbc852feebf632ff1c1c
Emilio Platzer <emilioplatzer@gmail.com>
2018-05-26 16:56:21 -0300

    Primera versión sin parámetros

---
+++ src/devel-sync.ts
@@ -0,0 +1,49 @@
+import * as fs from "fs-extra"
+import * as Path from "path"
+
+var params={
+    rootSource:'..',
+    rootTarget:'..',
+    sources:[
+        {path:'types.d.ts/modules', dest:'@types'},
+        'backend-plus'
+    ],
+    targets:[
+        'bas-ope'
+    ]
+}
+
+function copy(source:string,target:string):Promise<void>{
+    console.log('COPY',source,target)
+    return fs.copy(source,target,{recursive:true}).catch(function(err){
+        console.log('ERROR copying',source,target)
+        console.log(err);
+    })
+}
+
+var copyChain=Promise.resolve();
+
+function addToCopyChain(source:string,target:string){
+    copyChain=copyChain.then(function(){
+        return copy(source,target)
+    });
+}
+
+function sync(){
+    params.sources.forEach(function(pathOrObject){
+        let {path, dest}=typeof pathOrObject === 'string'?{
+            path:pathOrObject,
+            dest:''
+        }:pathOrObject;
+        var sourcePath=Path.join(params.rootSource,path);
+        console.log('watching',path,dest?'(to:'+dest+')':'',sourcePath, Path.resolve(sourcePath))
+        fs.watch(sourcePath, {recursive:true}, function(event, fileName){
+            console.log(new Date().toLocaleString(), event, fileName);
+            params.targets.forEach(function(target){
+                addToCopyChain(Path.join(sourcePath,fileName),Path.join(params.rootTarget,target,'node_modules',dest,fileName))
+            });
+        })
+    })
+}
+
+sync();
