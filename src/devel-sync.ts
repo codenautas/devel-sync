2f8c7f3be17cceed50e668c9887d29bca1bb96bc
Emilio Platzer <emilioplatzer@gmail.com>
2019-03-17 00:18:23 -0300

    Devel sync solo copia el dist (de "files":[])

--- src/devel-sync.ts
+++ src/devel-sync.ts
@@ -34,23 +34,23 @@ function addToCopyChain(source:string,target:string){
 }
 
 function sync(params:Params){
-    params.sources.forEach(function(pathOrObject){
+    params.sources.forEach(async function(pathOrObject){
         let {path, dest}=typeof pathOrObject === 'string'?{
             path:pathOrObject,
             dest:pathOrObject
         }:pathOrObject;
         var sourcePath=Path.join(params.rootSource,path);
+        if(await fs.pathExists(Path.join(sourcePath,'dist'))){
+            sourcePath = Path.join(sourcePath, 'dist');
+            dest= Path.join(dest, 'dist');
+        }
         console.log('watching',path,dest?'(to:'+dest+')':'',sourcePath, Path.resolve(sourcePath))
         fs.watch(sourcePath, {recursive:true}, function(event, fileName){
             console.log(new Date().toLocaleString(), event, fileName);
             params.targets.forEach(function(target){
                 if(fileName && target!=pathOrObject && !params.exclude.some(prefix=>fileName.startsWith(prefix))){
-                    var targetPath
-                    if(typeof target==="string"){
-                        targetPath=Path.join(params.rootTarget,target,'node_modules',dest,fileName)
-                    }else{
-                        targetPath=Path.join(target.absolute,'node_modules',dest,fileName);
-                    }
+                    var targetPath = typeof target==="string"?Path.join(params.rootTarget,target):target.absolute;
+                    targetPath = Path.join(targetPath, 'node_modules', dest, fileName);
                     addToCopyChain(Path.join(sourcePath,fileName),targetPath)
                 }
             });
